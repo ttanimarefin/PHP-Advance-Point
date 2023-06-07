0c5b1d57bb552ca29c634c2afa994191931297cb
ttanimarefin <ttanimarefin@gmail.com>
2023-06-07 12:03:47 +0600

    basic loop

--- part1/loop.php
+++ part1/loop.php
@@ -29,4 +29,39 @@ for($i=10; $i>0; $i-=1){
     echo PHP_EOL;
 }
 
+echo "new \n";
+
+
+
+//while loop
+$i=0;
+while($i<5){
+echo $i;
+echo PHP_EOL;
+    $i++;
+}
+echo "===========\n";
+
+$k=0;
+while($k++ <5){
+    echo $k;
+    echo PHP_EOL;
+$k++;
+}
+echo "===========\n";
+
+#continue and Breack
+
+for($i=0; $i<20; $i++){
+    if($i>5){
+        continue;
+    
+    
+    echo $i;
+    
+}
+
+
+
+
 ?>
